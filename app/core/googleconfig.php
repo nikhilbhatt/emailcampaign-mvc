86f18a18978131cc5e695aea9e0e743b5c449041
Nikhil Bhatt <44102752+nikhilbhatt@users.noreply.github.com>
2020-03-02 15:41:36 +0530

    Update googleconfig.php

--- app/core/googleconfig.php
+++ app/core/googleconfig.php
@@ -8,8 +8,8 @@ if (session_status() == PHP_SESSION_NONE) {
     $google_client=new Google_Client();
      
     $google_client->setApplicationName("Email Campaign");
-    $google_client->setClientId("208036174903-l26l8qc0q3gdp3a0ui3p65g447lh8jfk.apps.googleusercontent.com");
-    $google_client->setClientSecret("vqwQwzMY2TUbbJIeP1OtGy72");
+    $google_client->setClientId(YOUR_ID);
+    $google_client->setClientSecret(YOUR_SECREY);
     $google_client->setRedirectUri('http://localhost/googlelogin');
     $google_client->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
 ?>
